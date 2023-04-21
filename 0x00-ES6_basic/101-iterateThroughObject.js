f5c78ee9ebc42cdb1a4f85e9a2f31f44ba14b40d
warengeorge <georgeomiebi@gmail.com>
2023-04-21 13:00:50 +0100

    modified script

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return Array.from(reportWithIterator).join(' | ');
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+
+  return employees.join(' | ');
 }
