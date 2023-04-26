cbf8755c54a5392cece192fc9c596d0bb6ca191b
warengeorge <georgeomiebi@gmail.com>
2023-04-26 01:47:38 +0100

    Added function getListStudentIds

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * retrieves ids from a list of students
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String,
+ * }[]} students - The list of students.
+ * @author Johnkennedy Ogu <https://github.com/mathjken>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
