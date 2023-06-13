b7fcce52d88c71ca7795cb6af5a11b500a3aa489
warengeorge <georgeomiebi@gmail.com>
2023-06-13 23:44:25 +0100

    Added 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display messages on the STDOUT.
+ * @param {String} msg message to display.
+ * @author Tamunomiebi George <https://github.com/warengeorge>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
